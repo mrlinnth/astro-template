faa597a607b8aa7c085b3fd79649720e81cc1cfc
yan <yanlinnthor@evolxit.com>
2024-10-22 18:59:41 +0630

    add env variables

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
+interface ImportMetaEnv {
+  readonly SECRET_PASSWORD: string;
+  readonly PUBLIC_ANYBODY: string;
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
