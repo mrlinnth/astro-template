ec49ad3932150c0ec59a1901d6a1f9ca334a4065
Yan <yanlinnthor@evolxit.com>
2024-10-25 11:58:40 +0630

    update ssg

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), react()],
+  output: "hybrid",
+  integrations: [tailwind(), react()],
 });
