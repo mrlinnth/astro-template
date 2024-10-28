9b3fb0897f702af4c49cd3da8a1b9c63b8d8370e
Yan <yanlinnthor@evolxit.com>
2024-10-28 12:31:49 +0630

    install netlify adpater

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,11 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
+  adapter: netlify(),
   integrations: [tailwind(), react()],
 });
