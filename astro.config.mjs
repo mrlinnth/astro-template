bd92a40b441e869665e8974c1f63b1a48fe4ef02
Yan <yanlinnthor@evolxit.com>
2024-10-28 12:37:38 +0630

    uninstall netlify and install nodejs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,13 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 
-import netlify from "@astrojs/netlify";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  adapter: netlify(),
+  adapter: node({
+    mode: "standalone",
+  }),
   integrations: [tailwind(), react()],
 });
